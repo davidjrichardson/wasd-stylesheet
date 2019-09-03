4c2b31be79273fe0f4169b1efeb451f1e154fb85
David Richardson <david@tankski.co.uk>
2019-09-03 11:08:38 +0100

    Fix issues with CSS & open graph

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
 require('./wasd.scss');
-
-function easing(t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t };
-
-(function() {
-  let clock = document.getElementById('stopwatch-hand').getBoundingClientRect().bottom;
-  var bodyRect = document.body.getBoundingClientRect().y;
-  var clockBottom = clock - bodyRect;
-
-  document.addEventListener('scroll', function(event) {
-    var angle = easing((clockBottom - window.scrollY) / clockBottom);
-    angle = 360 - (360 * angle);
-
-    document.documentElement.style.setProperty('--stopwatch-rotation',  angle + 'deg');
-  });
-})();
